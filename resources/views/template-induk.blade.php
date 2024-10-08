3aa1412ea94234421ebba2368e1e59313c3466f2
Amirol Zolkifli <sitehandy@gmail.com>
2024-10-08 16:19:49 +0800

    Update latest changes

--- resources/views/template-induk.blade.php
+++ resources/views/template-induk.blade.php
@@ -138,5 +138,6 @@
         <script src="{{ asset('themes/sbadmin') }}/assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="{{ asset('themes/sbadmin') }}/js/datatables-simple-demo.js"></script>
+        @stack('js-script')
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,32 @@
 <?php
 
-use App\Http\Controllers\Authenticate\LoginController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\GuestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\PerkaraController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Authenticate\LoginController;
 
 Route::get('/', [GuestController::class, 'homepage']);
 // Route::get(uri, function);
 Route::get('/login', [LoginController::class, 'borangLogin']);
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/profil', [ProfilController::class, 'index']);
+
+// Paparkan senarai perkara
+// Route::get('/perkara', [PerkaraController::class, 'index']);
+// // Paparkan borang tambah perkara baru
+// Route::get('/perkara/create', [PerkaraController::class, 'create']);
+// // Dapatkan data daripada borang tambah perkara baru
+// Route::post('/perkara/create', [PerkaraController::class, 'store']);
+// // Paparkan borang edit perkara berdasarkan ID
+// Route::get('/perkara/{id}/edit', [PerkaraController::class, 'edit']);
+// // Dapatkan data daripada borang edit perkara
+// Route::patch('/perkara/{id}', [PerkaraController::class, 'update']);
+// // Hapus perkara berdasarkan ID
+// Route::delete('/perkara/{id}', [PerkaraController::class, 'destroy']);
+
+Route::resource('perkara', PerkaraController::class);
+
+
